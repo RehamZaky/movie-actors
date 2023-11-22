746f830d81c57abf68e9d23de4d0cb426fb56adf
RehamZaky <rehamzaky36@gmail.com>
2023-11-22 13:48:15 +0200

    Add splash screen animation and global colors for the app

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,12 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_assessment_task/popular/screens/popular.dart';
+import 'package:flutter_assessment_task/shared/style/global_colors.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MoviePopularScreen(),
+        ),
+      );
+    });
     return Scaffold(
-      body: Container(),
+      backgroundColor: GlobalColors.backgroundColor,
+      body: Center(child: Lottie.asset('assets/lottie/splash.json')),
     );
   }
 }

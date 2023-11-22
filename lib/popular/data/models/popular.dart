ce0e39831c4e5d4fccab6a93a51a0157642dd0f1
RehamZaky <rehamzaky36@gmail.com>
2023-11-22 12:22:50 +0200

    convert from json to popular model

--- lib/popular/data/models/popular.dart
+++ lib/popular/data/models/popular.dart
@@ -1,30 +1,38 @@
-
 import 'package:flutter_assessment_task/popular/data/models/popular_for.dart';
 import 'package:flutter_assessment_task/shared/data/models/department.dart';
 
 class Popular {
-    bool adult;
-    int gender;
-    int id;
-    KnownForDepartment knownForDepartment;
-    String name;
-    String originalName;
-    double popularity;
-    String profilePath;
-    List<KnownFor> knownFor;
+ late bool adult;
+ late int gender;
+ late int id;
+ late KnownForDepartment knownForDepartment;
+ late String name;
+ late String originalName;
+ late double popularity;
+ late String profilePath;
+ late List<KnownFor> knownFor;
 
-    Popular({
-        required this.adult,
-        required this.gender,
-        required this.id,
-        required this.knownForDepartment,
-        required this.name,
-        required this.originalName,
-        required this.popularity,
-        required this.profilePath,
-        required this.knownFor,
-    });
+  Popular({
+    required this.adult,
+    required this.gender,
+    required this.id,
+    required this.knownForDepartment,
+    required this.name,
+    required this.originalName,
+    required this.popularity,
+    required this.profilePath,
+    required this.knownFor,
+  });
 
+  Popular.fromJson(dynamic json) {
+    adult = json['adult'];
+    gender = json['gender'];
+    id = json['id'];
+    knownForDepartment = json['knownForDepartment'];
+    name = json['name'];
+    originalName = json['originalName'];
+    popularity = json['popularity'];
+    profilePath = json['profilePath'];
+    knownFor = json['knownFor'];
+  }
 }
-
-

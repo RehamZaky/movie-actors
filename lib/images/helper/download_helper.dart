12d5d0a1ea24a64f6c4c30cac5939aa2ef5bb087
RehamZaky <rehamzaky36@gmail.com>
2023-11-23 12:18:21 +0200

    Download helper class to test

--- lib/images/helper/download_helper.dart
+++ lib/images/helper/download_helper.dart
@@ -6,14 +6,16 @@ import 'package:path_provider/path_provider.dart';
 
 String fileName = 'image.jpg';
 
-void openFile(String url) async {
+ class DownloadHelper{
+
+ static openFile(String url) async {
   final file = await downloadFile(url);
   if (file == null) return;
 
   OpenFile.open(file.path);
 }
 
-Future<File?> downloadFile(String url) async {
+ static Future<File?>  downloadFile(String url) async {
   try {
     final response = await Dio().get(url,
         options: Options(
@@ -31,7 +33,8 @@ Future<File?> downloadFile(String url) async {
   }
 }
 
-Future<String> getDirectory() async {
+static Future<String>  getDirectory() async {
   final directory = await getDownloadsDirectory();
   return '${directory!.path}/$fileName';
+}
 }
